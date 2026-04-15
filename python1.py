5ec7720066f6462415b23092ff47b71c21472dd8
Numan115 <muhammadnuman.web@gmail.com>
2026-04-15 10:18:42 -0700

    Update python1.py

--- python1.py
+++ python1.py
@@ -4,3 +4,35 @@ def string_ops(s):
     return reversed_s, every_second
 
 print(string_ops("hello"))
+
+
+
+
+def odd_indices(lst):
+    return [i for i, val in enumerate(lst) if val % 2 != 0]
+
+print(odd_indices([10,11,12,13,14]))
+
+
+def rotate_list(lst, k):
+    k = k % len(lst)
+    return lst[-k:] + lst[:-k]
+
+print(rotate_list([1,2,3,4,5], 2))
+
+
+def list_to_dict(keys, values):
+    return dict(zip(keys, values))
+
+print(list_to_dict(["a","b","c"], [1,2,3]))
+
+
+def manual_iteration(lst):
+    it = iter(lst)
+    while True:
+        try:
+            print(next(it))
+        except StopIteration:
+            break
+
+manual_iteration([1,2,3,4])
